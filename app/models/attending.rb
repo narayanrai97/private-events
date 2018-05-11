287a2d1a18cf3aaf750f51f5af5288667c664a01
Narayan Rai <narayanrai97@gmail.com>
2018-05-11 19:30:26 +0000

    Disable past events attend button

--- app/models/attending.rb
+++ app/models/attending.rb
@@ -1,5 +1,8 @@
 class Attending < ApplicationRecord
     belongs_to :attendee, class_name: "User"
     belongs_to :attended_event, class_name: "Event"
+    
     validates :attendee_id, uniqueness: { scope: :attended_event_id } #Cruz helped
+    # I believed something like this can also be used to make it unique
+    # add_index :attendings, [:attendee_id, :attended_event_id], unique: true
 end
