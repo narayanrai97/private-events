823a803f00de3947db186a72ba5ce5e1c28d7cfb
Narayan Rai <narayanrai97@gmail.com>
2018-05-11 16:23:46 +0000

    Add a few callbacks on User and Event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
     default_scope -> { order(:date) } 
     
-    belongs_to :creator,    :class_name => "User"
+    belongs_to :creator,    :class_name => "User", touch: true #touch: true for CALLBACK
     has_many   :attendings, foreign_key: :attended_event_id
     has_many   :attendees,  through: :attendings
     # has_many :attendees, -> { distinct }, through: :attendings
@@ -12,4 +12,17 @@ class Event < ApplicationRecord
     
     scope :upcoming_events, -> { where('date >= ?', Time.zone.now) }
     scope :past_events,     -> { where('date < ?', Time.zone.now) }
+    
+    # Callbacks
+    after_destroy :log_destroy_action
+    
+    after_touch do
+        puts 'An Event was touched'
+    end
+    
+    private
+        # Callbacks
+        def log_destroy_action
+            puts 'Event destroyed'
+        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,38 @@
 class User < ApplicationRecord
     has_many :events, dependent: :destroy,
                       foreign_key: :creator_id
+    after_touch :log_when_events_or_user_touched #callbacks
+    
     has_many :attendings, foreign_key: :attendee_id
     has_many :attended_events, through: :attendings
     
     validates :name,  presence: true, length: { maximum: 25 }, uniqueness: true
-    validates :email, presence: true, length: { maximum: 25 }, uniqueness: true
-    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-    # def prev_events
-    #   attended_events.where('date < ?', Time.zone.now) 
-    # end
+    after_initialize do |user|
+        puts "You have initialized an object!"
+    end
+    
+    after_find do |user|
+        puts "You've found an object!"
+    end
+    
+    before_validation :normalize_name_email, on: [:create, :update] #callback
+   
+   private
+    
+    # Callbacks
+    def normalize_name_email
+        self.name = name.downcase.titleize
+        self.email = email.downcase if email.present?
+    end
+    
+    def log_when_events_or_user_touched
+        puts 'Event/User was touched'
+    end
 end
